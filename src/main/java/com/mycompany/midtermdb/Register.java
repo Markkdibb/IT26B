2a775c9625d199e962116c62614233ed84dcef43
markkdibb <marcdavetangaro0@gmail.com>
2026-04-12 13:42:38 +0800

    Register Window

--- src/main/java/com/mycompany/midtermdb/Register.java
+++ src/main/java/com/mycompany/midtermdb/Register.java
@@ -8,4 +8,19 @@ package com.mycompany.midtermdb;
  *
  * @author LENOVO
  */
+import javax.swing.*;
+import java.awt.*;
 
+public class Register extends JFrame {
+
+    Register() {
+        setTitle("Register");
+        setSize(450, 380);
+        setLayout(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+
+        setVisible(true);
+    }
+}
